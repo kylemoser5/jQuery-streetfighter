35252d6cfb054e96afe1e4cf3a5cde93ddff2d2c
kyle moser <kylemoser5@gmail.com>
2016-09-14 12:54:48 -0400

    addition of javascript

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$('.ryu').mouseenter(function(){
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function(){
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function(){
+		alert('HI');
+	})
+	.mouseup(function(){
+		alert('BYE');
+	})
+});
+
